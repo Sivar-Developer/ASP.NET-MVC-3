8bd2b3b939074d2a792f69db37ec25ce571e18fd
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-05 00:33:34 +0300

    Passing data to view

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,11 +13,16 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            // return View(movie);
+
+            // ViewData["Movie"] = movie;
+            // ViewBag.Movie = movie;
+
+            return View(movie);
+
             // return Content("Hello World!");
             // return HttpNotFound();
             // return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
+            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
         }
 
         public ActionResult Edit(int movieId)
